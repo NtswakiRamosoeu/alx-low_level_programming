601685f051673cdec3599d71f86b3d90db1fde0f
NtswakiRamosoeu <ntswakiramosoe@gmail.com>
2023-03-19 14:29:08 -0700

    5-print_numbers.c kjjhjjhk jhjhbkijjwhkw kwjilhikjsk;wldswdwde jhjuttrertyhrfty qqsswswd jbjhhgcfgyuyjcddhfgdggdsdsdgahgfchgwsfgwhjfsgwfswsghwgvwfcdswhdcwhxhw git commit -m 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - single digit numbers of base 10 starting from 0
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int s;
+
+	for (s = 0; s < 10; s++)
+		printf("%d", s);
+	printf("\n");
+	return (0);
+}
