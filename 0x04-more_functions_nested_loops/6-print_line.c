62efd8f36578e245072f2631f8c942ca465cac00
NtswakiRamosoeu <ntswakiramosoe@gmail.com>
2023-03-23 14:26:53 -0700

    draw lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_line - draws straight line
+ * @n: number of time the line should be drawn
+ */
+void print_line(int n)
+{
+	int p;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (p = 1; p <= n; p++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
